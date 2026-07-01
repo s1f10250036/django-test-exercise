6ac2f3c0b351d299e3e5ececf4292983449f10dd
1f10250036 <s1f102500360@iniad.org>
2026-07-02 04:44:57 +0900

    詳細ページを追加

--- todo/views.py
+++ todo/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
 
-# Create your views here.
 def index(request):
-    if request.method == 'POST':
-        task = Task(
-            title=request.POST['title'],
-            due_at=make_aware(parse_datetime(request.POST['due_at']))
-        )
-        task.save()
-        
-    if request.GET.get('order') == 'due':
-        tasks = Task.objects.order_by('due_at')
-    else:
-        tasks = Task.objects.order_by('-posted_at')
-    context = {
-        'tasks': tasks
-    }
+    tasks = Task.objects.all()
+    context = {'tasks': tasks}
     return render(request, 'todo/index.html', context)
+
+def detail(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+
+    context = {
+        'task': task,
+    }
+    return render(request, 'todo/detail.html', context)
